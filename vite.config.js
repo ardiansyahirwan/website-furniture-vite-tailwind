9738279375df5946d37289eafc154cd3bc228d0d
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2022-07-04 20:24:10 +0700

    add public path

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   plugins: [
     vue(),
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
-    })
+    }),
 ]
 })
